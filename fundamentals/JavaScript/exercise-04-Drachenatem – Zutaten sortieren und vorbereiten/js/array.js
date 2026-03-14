3ad59218647b8ad6e7cb980441c0534936b7c260
Daniele Padilha <reitzdaniele@gmail.com>
2026-03-14 19:51:27 +0100

    Sort Ingrediets

---
+++ fundamentals/JavaScript/exercise-04-Drachenatem – Zutaten sortieren und vorbereiten/js/array.js
@@ -0,0 +1,7 @@
+let zutaten = ['Huhn', 'Currypulver', 'Kokosmilch', 'Ingwer', 'Zwiebel', 'Reis', 'Zitronesaft', 'Chili'];
+let mengen = [ 1, 10, 250, 2, 1, 200, 20, 0.5]
+let einheiten = ['Stk', 'g', 'ml', 'ml', 'Stk', 'g', 'ml', 'g']
+
+console.log(zutaten[0]);
+console.log(mengen[0]);
+console.log(einheiten[0]);
